49fd49c0450817177a1469de5221198b710f3826
mbootland <matthew.bootland@gmail.com>
2024-09-13 12:20:34 +0900

    Squish me

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe TweetsController, type: :controller do
   describe 'GET #index' do
-    it 'returns a successful response' do
+    let(:user) { User.create(username: 'Boots') }
+    let!(:tweet) { Tweet.create(content: 'This is a test tweet', user: user) }
+
+    it 'returns a successful response with tweets' do
       get :index
       expect(response).to be_successful
+      expect(Tweet.all.count).to eq(1)
     end
   end
 end
